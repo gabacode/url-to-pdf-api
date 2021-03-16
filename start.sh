9ab1515d9076c824ccb94a512a7c0422312ca038
Marcello Vetro <vetro@olomedia.it>
2021-03-16 10:35:22 +0100

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 # Installa i node_modules
-npm install
+yarn install
 
 # start cron for time pdf and start sever
 #(cron -f &) &&  nodejs index.js
